439c0c805b74fc968b88b3b8f4fa3e5b34af101a
Chromatischer <chromatischer@hildania.de>
2025-02-12 11:46:43 +0100

    removed Utils!

--- Utils/Time/timeUtils.lua
+++
@@ -1,9 +0,0 @@
----truns 10.5 hours into 10h and 30m
----@param fraction number the number of hours
----@return table time_information h are the hours and m are minutes
-function fractionOfHoursToHoursAndMinutes(fraction)
-    hours = math.floor(fraction) -- Get the whole number of hours
-    remainingFraction = fraction - hours -- Get the remaining fraction of an hour
-    minutes = math.floor(remainingFraction * 60) -- Calculate the number of minutes
-    return {h=hours, m=minutes}
-end
--- Utils/Vectors/Vectors.lua
+++
@@ -1,2 +0,0 @@
-require("Utils.Vectors.vec2")
-require("Utils.Vectors.vec3")
