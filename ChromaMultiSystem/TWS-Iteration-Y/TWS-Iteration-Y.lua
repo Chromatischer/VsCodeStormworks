08203e46a0636abd34f62a0912dd0f4f1f0998d2
Chromatischer <chromatischer@hildania.de>
2025-06-11 22:50:46 +0200

    Display Code II

--- ChromaMultiSystem/TWS-Iteration-Y/TWS-Iteration-Y.lua
+++ ChromaMultiSystem/TWS-Iteration-Y/TWS-Iteration-Y.lua
@@ -70,6 +70,11 @@ tracks = {} ---@type Track[]
 
 intercepts = {} --TODO: Maybe?
 
+-- Display variables
+renderDepression = 20 ---@type integer Pixel amount the vessel icon should be depressed by on the rectangular screen portion
+dirUp = 0 ---@type number Radians Direction that is Upwards for the radar
+mapDiameter = 10 ---@type number Total diameter that the screen should display in km
+
 ticks = 0
 function onTick()
     ticks = ticks + 1
@@ -107,5 +112,43 @@ function onTick()
 end
 
 function onDraw()
-    --TODO: Draw that shit to the screen!
+    Swidth, Sheight = screen.getWidth(), screen.getHeight() --224x160
+    --! Ok so this project aims for: 7x5 scaled to 3x2 monitors
+
+    -- Split the screen into two parts: one rectangular and one to the side.
+    -- Split into 64 to the right and 160 on the side
+    screen.setColor(100, 0, 0, 128)
+    screen.drawRect(0, 0, 63, 160)
+
+    -- The rectangular part is for the main radar
+    screen.setColor(0, 100, 0, 128)
+    screen.drawRect(64, 0, 160, 160)
+
+    -- Set the point at which the vessel is to be rendered
+    radarMidPointX = 144 -- 64 + 160 / 2 (The midpoint of the larger rectangle)
+    radarMidPointY = 80 + renderDepression
+
+    ---Translates a Worldspace Position to a Screenspace position
+    ---@param world Vec2 Worldposition
+    ---@param center Vec2 Worldposition where the Map is centered
+    ---@param updir number (rad) The direction that should be upward on screen
+    ---@param scale number (m) Diameter of the entire Map
+    ---@return Vec2 Vec2 Screenspace position
+    function transformWS(world, center, updir, scale)
+        relative = subtract(world, center) --The relative position of the world position to the center
+        --[[
+        distance = vec2length(relative) --Distance to the Worldposition in meters
+        angle = math.atan(relative.y, relative.x) --Angle to the relative Position
+        rotatedAngle = angle - updir --Rotated to be updir facing upwards
+        transformScalar(relative, rotatedAngle, distance / scale)
+
+        Transformed into Screenspace by:
+        1. Converting into relative position
+        2. Rotating to be facing the correct direction
+        3. Scaled by the proportion the distance to the position takes on screen at the current scale
+        And now as a single liner because I can:
+        ]]--
+        return transformScalar(relative, math.atan(relative.y, relative.x) - updir, vec2length(relative) / scale)
+    end
+    --TODO: Render tracks
 end
