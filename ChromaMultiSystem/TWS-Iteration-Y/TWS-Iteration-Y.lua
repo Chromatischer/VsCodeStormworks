10fafc62ab768d5ad149df3d3d9d3f1fd4ed0546
Chromatischer <chromatischer@hildania.de>
2025-06-13 14:18:54 +0200

    WIP

--- ChromaMultiSystem/TWS-Iteration-Y/TWS-Iteration-Y.lua
+++ ChromaMultiSystem/TWS-Iteration-Y/TWS-Iteration-Y.lua
@@ -5,37 +5,6 @@
 --- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
 --- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
 
---[====[ EDITABLE SIMULATOR CONFIG - *automatically removed from the F7 build output ]====]
----@section __LB_SIMULATOR_ONLY__
-do
-    ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
-    simulator = simulator
-    simulator:setScreen(1, "3x3")
-    simulator:setProperty("ExampleNumberProperty", 123)
-
-    -- Runs every tick just before onTick; allows you to simulate the inputs changing
-    ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
-    ---@param ticks     number Number of ticks since simulator started
-    function onLBSimulatorTick(simulator, ticks)
-
-        -- touchscreen defaults
-        local screenConnection = simulator:getTouchScreen(1)
-        simulator:setInputBool(1, screenConnection.isTouched)
-        simulator:setInputNumber(1, screenConnection.width)
-        simulator:setInputNumber(2, screenConnection.height)
-        simulator:setInputNumber(3, screenConnection.touchX)
-        simulator:setInputNumber(4, screenConnection.touchY)
-
-        -- NEW! button/slider options from the UI
-        simulator:setInputBool(31, simulator:getIsClicked(1))       -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
-        simulator:setInputNumber(31, simulator:getSlider(1))        -- set input 31 to the value of slider 1
-
-        simulator:setInputBool(32, simulator:getIsToggled(2))       -- make button 2 a toggle, for input.getBool(32)
-        simulator:setInputNumber(32, simulator:getSlider(2) * 50)   -- set input 32 to the value from slider 2 * 50
-    end;
-end
----@endsection
-
 --#region CH Layout
 -- N-1: Global Scale
 -- N-2: GPS X
@@ -58,7 +27,6 @@ require("Vectors.Vectors")
 require("Radar.BestTrackAlgorithm")
 require("Radar.radarToGlobalCoordinates")
 
-
 worldPos = Vec3(0, 0, 0) --TODO: Read in and remove this redundant declaration!
 
 rawRadarData = {}
@@ -77,66 +45,72 @@ mapDiameter = 10 ---@type number Total diameter that the screen should display i
 
 ticks = 0
 function onTick()
-    ticks = ticks + 1
+	ticks = ticks + 1
 
-    dataOffset = 11
-    boolOffset = 5
-    for i = 0, 3 do
-        distance = input.getNumber(i * 4 + dataOffset)
-        targetDetected = input.getBool(i + boolOffset)
-        timeSinceDetected = input.getNumber(i * 4 + 3 + dataOffset)
-        relPos = radarToRelativeVec3(distance, input.getNumber(i * 4 + 1 + dataOffset), input.getNumber(i * 4 + 2 + dataOffset), compas, input.getNumber(13))
-        tgt = rawRadarData[i + 1]
+	dataOffset = 11
+	boolOffset = 5
+	for i = 0, 3 do
+		distance = input.getNumber(i * 4 + dataOffset)
+		targetDetected = input.getBool(i + boolOffset)
+		timeSinceDetected = input.getNumber(i * 4 + 3 + dataOffset)
+		relPos = radarToRelativeVec3(
+			distance,
+			input.getNumber(i * 4 + 1 + dataOffset),
+			input.getNumber(i * 4 + 2 + dataOffset),
+			compas,
+			input.getNumber(13)
+		)
+		tgt = rawRadarData[i + 1]
 
-        if timeSinceDetected ~= 0 then
-            -- Using target smoothing like Smithy
-            -- new = old + ((new - old) / n)
-            rawRadarData[i + 1] = addVec3(tgt, scaleDivideVec3(subVec3(relPos, tgt), timeSinceDetected))
+		if timeSinceDetected ~= 0 then
+			-- Using target smoothing like Smithy
+			-- new = old + ((new - old) / n)
+			rawRadarData[i + 1] = addVec3(tgt, scaleDivideVec3(subVec3(relPos, tgt), timeSinceDetected))
 
-            -- Using recursive averaging
-            -- new = ((old * n-1) + new) / n
-            -- rawRadarData[i + 1] = tgt and scaleDivideVec3(addVec3(relPos, scaleVec3(tgt, timeSinceDetected - 1)), timeSinceDetected) or relPos
-        elseif tgt then --Check that there is a contact at the postion.
-            table.insert(contacts, addVec3(worldPos, tgt))
-            rawRadarData[i + 1] = nil
-        end
-    end
+			-- Using recursive averaging
+			-- new = ((old * n-1) + new) / n
+			-- rawRadarData[i + 1] = tgt and scaleDivideVec3(addVec3(relPos, scaleVec3(tgt, timeSinceDetected - 1)), timeSinceDetected) or relPos
+		elseif tgt then --Check that there is a contact at the postion.
+			table.insert(contacts, addVec3(worldPos, tgt))
+			rawRadarData[i + 1] = nil
+		end
+	end
 
-    tracks = updateTrackT(tracks) ---@type Track[]
+	tracks = updateTrackT(tracks) ---@type Track[]
 
-    if #contacts ~= 0 then -- Now possible through the use of the Hungarian algorithm
-        -- This is the complete solution: updating, deleting and creating in one! Amazing
-        tracks = hungarianTrackingAlgorithm(contacts, tracks, MAX_SEPERATION, LIFESPAN * 60, {})
-        contacts = {} -- Clear contacts!
-    end
+	if #contacts ~= 0 then -- Now possible through the use of the Hungarian algorithm
+		-- This is the complete solution: updating, deleting and creating in one! Amazing
+		tracks = hungarianTrackingAlgorithm(contacts, tracks, MAX_SEPERATION, LIFESPAN * 60, {})
+		contacts = {} -- Clear contacts!
+	end
 end
 
 function onDraw()
-    Swidth, Sheight = screen.getWidth(), screen.getHeight() --224x160
-    --! Ok so this project aims for: 7x5 scaled to 3x2 monitors
+	Swidth, Sheight = screen.getWidth(), screen.getHeight() --224x160
+	--! Ok so this project aims for: 7x5 scaled to 3x2 monitors
 
-    -- Split the screen into two parts: one rectangular and one to the side.
-    -- Split into 64 to the right and 160 on the side
-    screen.setColor(100, 0, 0, 128)
-    screen.drawRect(0, 0, 63, 160)
+	-- Split the screen into two parts: one rectangular and one to the side.
+	-- Split into 64 to the right and 160 on the side
+	screen.setColor(100, 0, 0, 128)
+	screen.drawRect(0, 0, 63, 160)
 
-    -- The rectangular part is for the main radar
-    screen.setColor(0, 100, 0, 128)
-    screen.drawRect(64, 0, 160, 160)
+	-- The rectangular part is for the main radar
+	screen.setColor(0, 100, 0, 128)
+	screen.drawRect(64, 0, 160, 160)
 
-    -- Set the point at which the vessel is to be rendered
-    radarMidPointX = 144 -- 64 + 160 / 2 (The midpoint of the larger rectangle)
-    radarMidPointY = 80 + renderDepression
+	-- Set the point at which the vessel is to be rendered
+	radarMidPointX = 144 -- 64 + 160 / 2 (The midpoint of the larger rectangle)
+	radarMidPointY = 80 + renderDepression
 
-    ---Translates a Worldspace Position to a Screenspace position
-    ---@param world Vec2 Worldposition
-    ---@param center Vec2 Worldposition where the Map is centered
-    ---@param updir number (rad) The direction that should be upward on screen
-    ---@param scale number (m) Diameter of the entire Map
-    ---@return Vec2 Vec2 Screenspace position
-    function transformWS(world, center, updir, scale)
-        relative = subtract(world, center) --The relative position of the world position to the center
-        --[[
+	---Translates a Worldspace Position to a Screenspace position
+	---@param world Vec2 Worldposition
+	---@param center Vec2 Worldposition where the Map is centered
+	---@param updir number (rad) The direction that should be upward on screen
+	---@param scale number (m) Diameter of the entire Map
+	---@return Vec2 Vec2 Screenspace position
+	function transformWS(world, center, updir, scale)
+		relative = subtract(world, center) --The relative position of the world position to the center
+		--[[
         distance = vec2length(relative) --Distance to the Worldposition in meters
         angle = math.atan(relative.y, relative.x) --Angle to the relative Position
         rotatedAngle = angle - updir --Rotated to be updir facing upwards
@@ -147,8 +121,9 @@ function onDraw()
         2. Rotating to be facing the correct direction
         3. Scaled by the proportion the distance to the position takes on screen at the current scale
         And now as a single liner because I can:
-        ]]--
-        return transformScalar(relative, math.atan(relative.y, relative.x) - updir, vec2length(relative) / scale)
-    end
-    --TODO: Render tracks
+        ]]
+		--
+		return transformScalar(relative, math.atan(relative.y, relative.x) - updir, vec2length(relative) / scale)
+	end
+	--TODO: Render tracks
 end
